101562b665bc134b98d6c1c21d41c1fdab85327b
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-28 10:02:06 -0400

    commit tree c5560f0739f7e808af003e4733e0ebb29b0e8624

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,14 +3,46 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3001/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    uneatenSushis: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      const four = data.slice(0, 4)
+      console.log(data, four)
+      this.setState({sushis: data, uneatenSushis: four})
+    })
+  }
+
+  getSushi = () => {
+    const four = data.slice(0, 4)
+    console.log(data, four)
+    this.setState(state => {
+      const newSushis = state.sushis
+      const newUneaten = newSushis.splice(0,4)
+      return {
+      sushis: newSushis, 
+      uneatenSushis: newUneaten
+    }})
+  }
+
+  eatSushi = (e) => {
+    console.log(e.target.id)
+    this.setState({...this.state, uneatenSushis: this.state.uneatenSushis.filter(s => s.id != e.target.id)})
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer uneatenSushis={this.state.uneatenSushis} eatTheSushi={this.eatSushi} getTheSushi={this.getSushi}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,14 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  const sushisList = props.uneatenSushis.map(sushi => <Sushi sushi={sushi} eatTheSushi={props.eatTheSushi} />)
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {sushisList}
+        <MoreButton getTheSushi={props.getTheSushi}/>
       </div>
     </Fragment>
   )
